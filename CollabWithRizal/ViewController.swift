4dcc4234d8ee917bb78a56c0126e2608f270a2b2
muhammadtafanirabbani <ratafani@gmail.com>
2020-05-07 08:27:16 +0700

    first Commit

---
+++ CollabWithRizal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollabWithRizal
+//
+//  Created by Muhammad Tafani Rabbani on 07/05/20.
+//  Copyright © 2020 Muhammad Tafani Rabbani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
